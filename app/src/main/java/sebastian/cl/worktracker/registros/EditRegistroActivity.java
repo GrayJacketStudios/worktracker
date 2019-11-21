7fd98cf1223acfaee55e05f9d7ffde1246d49faa
Sebastian <grayjacketstudios@gmail.com>
2019-11-20 21:23:17 -0300

    Ultimos cambios al edit de registros

---
+++ app/src/main/java/sebastian/cl/worktracker/registros/EditRegistroActivity.java
@@ -0,0 +1,14 @@
+package sebastian.cl.worktracker.registros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditRegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_registro);
+    }
+}
